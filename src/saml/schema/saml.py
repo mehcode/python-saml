5dad05b00edea4571863149d89af7941bef3b986
Ryan Leckey <leckey.ryan@gmail.com>
2012-08-01 17:12:14 -0700

    Applied several layers of polish on existing data structures in saml.

--- src/saml/schema/saml.py
+++ src/saml/schema/saml.py
@@ -26,95 +26,73 @@
            CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
            SOFTWARE.
 """
-from .. import VERSION
-from .. import schema
 from uuid import uuid4
 from datetime import datetime
+from .. import schema
 
 
-class Type(schema.Type):
-    """Represents a type in this namespace."""
-    namespace = ("saml", "urn:oasis:names:tc:SAML:2.0:assertion")
+class Element(schema.Element):
+    class Meta:
+        namespace = ("saml", "urn:oasis:names:tc:SAML:2.0:assertion")
 
 
-class BaseIDAbstractType(Type):
-    """
-    Provides an extension point that allows applications to add new kinds of
-    identifiers.
+class BaseIDAbstractType(Element):
+    """Provides an extension point for identifiers.
     """
 
     ## The security or administrative domain that qualifies the name.
     name_qualifier = schema.Attribute(0, "NameQualifier")
 
-    ## Further qualifies a name with the name of a service provider or
-    ## affiliation of providers.
+    ## Further qualifies a name with the name of a service provider.
     sp_name_qualifier = schema.Attribute(1, "SPNameQualifier")
 
 
 class NameIDType(BaseIDAbstractType):
-    """
-    Used when an element serves to represent an entity by a string-valued name.
-    It is a more restricted form of identifier than the <BaseID> element and
-    is the type underlying both the <NameID> and <Issuer> elements.
-
-    In addition to the string content containing the actual identifier, it
-    provides the following optional attributes.
+    """Serves to represent an entity by a string-valued name.
     """
 
     class Format:
-        ## The interpretation of the attribute name is left to individual
-        ## implementations.
-        UNSPECIFIED = "urn:oasis:names:tc:SAML:1.0:nameid-format:unspecified"
+        """Enumerates accepted values for \ref NameID.format.
+        """
 
-        ## Indicates that the content of the element is in the form of an email
-        ## address.
-        EMAIL = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+        ## URI prefix for the values in this enumeration.
+        _PREFIX = "urn:oasis:names:tc:SAML:1.0:nameid-format:"
 
-        ## Indicates that the content of the element is in the form specified
-        ## for the contents of the <ds:X509SubjectName> element in the XML
-        ## Signature Recommendation [XMLSig].
-        X509 = "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName"
+        ## The interpretation is left to individual implementations.
+        UNSPECIFIED = "{}unspecified".format(_PREFIX)
 
-        ## Indicates that the content of the element is a Windows domain
-        ## qualified name.
-        WINDOWS = (
-            "urn:oasis:names:tc:SAML:1.1:nameid-format"
-            ":WindowsDomainQualifiedName")
+        ## Indicates a form of an email address.
+        EMAIL = "{}emailAddress".format(_PREFIX)
 
-        ## Indicates that the content of the element is in the form of a
-        ## Kerberos principal name using the format name[/instance]@REALM.
-        KEREBOS = "urn:oasis:names:tc:SAML:2.0:nameid-format:kerberos"
+        ## Is in the form specified by the X.509 recommendation [XMLSig].
+        X509 = "{}X509SubjectName".format(_PREFIX)
 
-        ## Indicates that the content of the element is the identifier of an
-        ## entity that provides SAML-based services.
-        ENTITY = "urn:oasis:names:tc:SAML:2.0:nameid-format:entity"
+        ## Is in the format of a Windows domain qualified name.
+        WINDOWS = "{}WindowsDomainQualifiedName".format(_PREFIX)
 
-        ## Indicates that the content of the element is a persistent opaque
-        ## identifier for a principal that is specific to an identity provider
-        ## and a service provider or affiliation of service providers.
-        PERSISTENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"
+        ## Is in the form of a Kerberos principal name.
+        KEREBOS = "{}kerberos".format(_PREFIX)
 
-        ## Indicates that the content of the element is an identifier with
-        ## transient semantics and SHOULD be treated as an opaque and
-        ## temporary value by the relying party.
-        TRANSIENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
+        ## Is the identifier of an entity that provides SAML-based services.
+        ENTITY = "{}entity".format(_PREFIX)
 
-    ## A URI reference representing the classification of string-based
-    ## identifier information.
+        ## Is a persistent opaque identifier for a principal.
+        PERSISTENT = "{}persistent".format(_PREFIX)
+
+        ## Is an identifier with transient semantics.
+        TRANSIENT = "{}transient".format(_PREFIX)
+
+    ## A URI classification of string-based identifier information.
     format = schema.Attribute(0, "Format", default=Format.UNSPECIFIED)
 
-    ## A name identifier established by a service provider or affiliation of
-    ## providers for the entity, if different from the primary name
-    ## identifier given in the content of the element.
+    ## A name identifier established by a service provider.
     sp_provided_id = schema.Attribute(1, "SPProvidedID")
 
 
 class NameID(NameIDType):
+    """Is the identifier used in various SAML assertion constructs.
     """
-    Is of type NameIDType (see Section 2.2.2), and is used in various SAML
-    assertion constructs such as the <Subject> and <SubjectConfirmation>
-    elements, and in various protocol messages.
-    """
+    pass
 
 
 ## \todo Element : <EncryptedID>
@@ -122,13 +100,10 @@ class NameID(NameIDType):
 
 
 class Issuer(NameIDType):
-    """
-    Provides information about the issuer of a SAML assertion or protocol
-    message.
+    """Represents the issuer of a SAML assertion or protocol message.
     """
 
-    ## Overriding the usual rule for this element's type, if no Format value is
-    ## provided with this element, then the value \ref ENTITY is in effect.
+    ## If no Format value is provided, then the value ENTITY is in effect.
     format = schema.Attribute(0, "Format", default=NameID.Format.ENTITY)
 
 
@@ -136,11 +111,8 @@ class Issuer(NameIDType):
 ## \todo Element    : <AssertionURIRef>
 
 
-class SubjectConfirmationData(Type):
-    """
-    Specifies additional data that allows the subject to be confirmed or
-    constrains the circumstances under which the act of subject confirmation
-    can take place.
+class SubjectConfirmationData(Element):
+    """Specifies constraints on allowing a subject to be confirmed.
     """
 
     ## \todo Support for KeyInfoConfirmationDataType
@@ -154,51 +126,41 @@ class SubjectConfirmationData(Type):
     ## A time instant at which the subject can no longer be confirmed.
     not_on_or_after = schema.DateTimeAttribute(1, "NotOnOrAfter")
 
-    ## A URI specifying the entity or location to which an attesting entity
-    ## can present the assertion.
+    ## URI specifying to which an attesting entity can present the assertion.
     recipient = schema.Attribute(2, "Recipient")
 
-    ## The ID of a SAML protocol message in response to which an attesting
-    ## entity can present the assertion.
+    ## ID of a SAML message to the entity can present the assertion to.
     in_response_to = schema.Attribute(3, "InResponseTo")
 
-    ## The network address/location from which an attesting entity can
-    ## present the assertion.
+    ## The network address to which the saml entity can present the assertion.
     address = schema.Attribute(4, "Address")
 
 
-class SubjectConfirmation(Type):
-    """
-    Provides the means for a relying party to verify the correspondence of the
-    subject of the assertion with the party with whom the relying party is
-    communicating.
+class SubjectConfirmation(Element):
+    """Provides the means for a relying party to verify the subject.
     """
 
     ## \todo class Method: Enumeration of values available
 
-    ## A URI reference that identifies a protocol or mechanism to be used to
-    ## confirm the subject.
+    ## URI reference that identifies a protocol to confirm the subject.
     method = schema.Attribute(0, "Method", required=True)
 
-    ## Identifies the entity expected to satisfy the enclosing subject
-    ## confirmation requirements.
-    id = schema.Element(1, BaseIDAbstractType)
+    ## Identifies the entity expected to satisfy the enclosed requirements.
+    id = BaseIDAbstractType(meta__index=1)
 
-    ## Additional confirmation information to be used by a specific
-    ## confirmation method.
-    data = schema.Element(2, SubjectConfirmationData)
+    ## Confirmation information and constraints.
+    data = SubjectConfirmationData(meta__index=2)
 
 
-class Subject(Type):
+class Subject(Element):
+    """The principal that is the subject of all statements in an assertion.
     """
-    Specifies the principal that is the subject of all of the (zero or more)
-    statements in the assertion.
-    """
+
     ## Identifies the subject.
-    id = schema.Element(0, BaseIDAbstractType)
+    id = BaseIDAbstractType(meta__index=0)
 
     ## Information that allows the subject to be confirmed.
-    confirm = schema.Element(1, SubjectConfirmation)
+    confirm = SubjectConfirmation(meta__index=1)
 
 
 ## \todo Element <EncryptedAssertion>
@@ -208,38 +170,35 @@ class Subject(Type):
 ## \todo Element <ProxyRestriction>
 ## \todo Element <Advice>
 
-class Statement(Type):
+
+class Statement(Element):
+    """An extension point for statements made about a subject.
     """
-    An extension point that allows other assertion-based applications to
-    reuse the SAML assertion framework.
-    """
+    pass
 
 
-class AuthnContext(Type):
-    """
-    Specifies the context of an authentication event. The element can
-    contain an authentication context class reference,
+class AuthenticationContext(Element):
+    """Specifies the context of an authentication event.
     """
 
-    class ClassRef:
-        """List of context classes defined by reference in SAML2."""
+    class Meta:
+        name = 'AuthnContext'
 
-        ## URI prefix.
+    class Reference:
+        """List of context classes defined by reference in SAML2.
+        """
+
+        ## URI prefix for the values in this enumeration.
         _PREFIX = "urn:oasis:names:tc:SAML:2.0:ac:classes:"
 
-        ## A principal is authenticated through the use of a provided IP
-        ## address.
+        ## Authenticated through the use of a provided IP address.
         INTERNET_PROTOCOL = "{}InternetProtocol".format(_PREFIX)
 
-        ## A principal is authenticated through the use of a provided IP
-        ## address, in addition to a username/password.
+        ## Authenticated through a provided IP address and username/password.
         INTERNET_PROTOCOL_PASSWORD = "{}InternetProtocolPassword".format(
             _PREFIX)
 
-        ## The principal has authenticated using a password to a local
-        ## authentication authority, in order to acquire a Kerberos ticket.
-        ## That Kerberos ticket is then used for subsequent network
-        ## authentication.
+        ## Authenticated using a Kerberos ticket.
         KERBEROS = "{}Kerberos".format(_PREFIX)
 
         ## Reflects no mobile customer registration procedures and an
@@ -263,14 +222,12 @@ class AuthnContext(Type):
         MOBILE_TWO_FACTOR_CONTRACT = "{}MobileTwoFactorContract".format(
             _PREFIX)
 
-        ## The Password class is applicable when a principal authenticates to
-        ## an authentication authority through the presentation of a password
-        ## over an unprotected HTTP session.
+        ## Authenticates through the presentation of a password over an
+        ## unprotected HTTP session.
         PASSWORD = "{}Password".format(_PREFIX)
 
-        ## The PasswordProtectedTransport class is applicable when a principal
-        ## authenticates to an authentication authority through the
-        ## presentation of a password over a protected session.
+        ## Authentication through the presentation of a password over a
+        ## protected session.
         PASSWORD_PROTECTED_TRANSPORT = "{}PasswordProtectedTransport".format(
             _PREFIX)
 
@@ -323,12 +280,11 @@ class AuthnContext(Type):
         ## protocol such as ADSL.
         PERSONAL_TELEPHONY = "{}PersonalTelephony".format(_PREFIX)
 
-        ## The principal authenticated via the means of the line number,
+        ## Authenticated via the means of the line number,
         ## a user suffix, and a password element.
         AUTHENTICATED_TELEPHONY = "{}AuthenticatedTelephony".format(_PREFIX)
 
-        ## The authentication was performed by means of Secure Remote Password
-        ## as specified in [RFC 2945].
+        ## Authenticated by means of Secure Remote Password s[RFC 2945].
         SECURE_REMOTE_PASSWORD = "{}SecureRemotePassword".format(_PREFIX)
 
         ## The principal authenticated by means of a client certificate,
@@ -338,23 +294,20 @@ class AuthnContext(Type):
         ## A principal authenticates through a time synchronization token.
         TIME_SYNC_TOKEN = "{}TimeSyncToken".format(_PREFIX)
 
-        ## The Unspecified class indicates that the authentication was
-        ## performed by unspecified means.
+        ## Indicates that the authentication means are unspecified.
         UNSPECIFIED = "{}Unspecified".format(_PREFIX)
 
     ## A URI reference identifying an authentication context class that
     ## describes the authentication context declaration that follows.
-    class_ref = schema.SimpleElement(
+
+    reference = schema.SimpleElement(
         index=0,
         name="AuthnContextClassRef",
         namespace=Type.namespace,
-        default=lambda: AuthnContext.ClassRef.UNSPECIFIED)
-
-    ## \todo <AuthnContextDecl> or <AuthnContextDeclRef>
-    ## \todo <AuthenticatingAuthority>
+        default=lambda: AuthenticationContext.Reference.UNSPECIFIED)
 
 
-class AuthnStatement(Statement):
+class AuthenticationStatement(Statement):
     """
     Describes a statement by the SAML authority asserting that the assertion
     subject was authenticated by a particular means at a particular time.
@@ -382,15 +335,19 @@ class AuthnStatement(Statement):
 
     ## The context used by the authenticating authority up to and including
     ## the authentication event that yielded this statement.
-    context = schema.Element(3, AuthnContext)
+    context = AuthenticationContext(meta__index=3)
 
 
-class Assertion(Type):
-    """Specifies the basic information that is common to all assertions."""
+class Assertion(Element):
+    """Specifies the basic information that is common to all assertions.
+    """
 
-    ## The version of this assertion. The identifier for the version of SAML
-    ## defined in this specification is "2.0".
-    version = schema.Attribute(0, "Version", required=True, default=VERSION)
+    ## The version of the schema used by this assertion.
+    version = schema.Attribute(
+        index=0,
+        name="Version",
+        required=True,
+        default=schema.VERSION)
 
     ## The identifier for this assertion.
     id = schema.Attribute(
@@ -399,35 +356,27 @@ class Assertion(Type):
         required=True,
         default=lambda: '_{}'.format(uuid4().hex))
 
-    ## The time instant of issue in UTC.
-    issue_instant = schema.DateTimeAttribute(
+    ## The time instanct of issue, in UTC, for this assertion.
+    issue_instant = schema.Attribute(
         index=2,
         name="IssueInstant",
         required=True,
         default=datetime.utcnow)
 
     ## The SAML authority that is making the claim(s) in the assertion.
-    issuer = schema.Element(3, Issuer, min_occurs=1)
+    issuer = Issuer(meta__index=3, meta__min_occurs=1)
 
     ## \todo Element <ds:Signature>
 
     ## The subject of the statement(s) in the assertion.
-    subject = schema.Element(4, Subject)
+    subject = Subject(meta__index=4)
 
-    ## Conditions that MUST be evaluated when assessing the validity of and/or
-    ## when using the assertion.
-    ## \todo conditions = schema.Element(Conditions)
-
-    ## Additional information related to the assertion that assists
-    ## processing in certain situations but which MAY be ignored by
-    ## applications that do not understand the advice or do not wish to make
-    ## use of it.
-    ## \todo advice = schema.Element(Advice)
+    ## \todo Element <Conditions>
+    ## \todo Element <Advice>
 
     ## The collection of statements asserted by this assertion.
-    statements = schema.Element(5, Statement, max_occurs=None)
+    statements = Statement(meta__index=5, meta__max_occurs=None)
 
-## \todo Element <AuthnStatement>
 
 ## \todo Element <SubjectLocality>
 ## \todo Element <AttributeStatement>

56fbbcb55326613349ea79924ad8f18e066b231a
Ryan Leckey <rleckey@concordusapps.com>
2013-03-17 14:17:56 -0700

    Commented out for now

--- src/saml/bindings.py
+++ src/saml/bindings.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function, unicode_literals, division
-import abc
-import base64
-import zlib
+# # -*- coding: utf-8 -*-
+# from __future__ import print_function, unicode_literals, division
+# import abc
+# import base64
+# import zlib
 
 
-class Redirect(object):
+# class Redirect(object):
 
-    @staticmethod
-    def receive(message):
-        return zlib.decompress(base64.b64decode(message), -15)
+#     @staticmethod
+#     def receive(message):
+#         return zlib.decompress(base64.b64decode(message), -15)
 
-    @staticmethod
-    def send(message):
-        return base64.b64encode(zlib.compress(message)[2:-4])}
+#     @staticmethod
+#     def send(message):
+#         return base64.b64encode(zlib.compress(message)[2:-4])}

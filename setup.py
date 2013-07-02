bb93f52b31d612995514ac2b545d623621f1bb5d
Ryan Leckey <rleckey@concordusapps.com>
2013-07-01 23:06:11 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='saml',
-    version='0.2.0',
+    version='0.2.1',
     description='A python interface to produce and consume Security Asserion Markup Language v2.0 (SAML2) messages.',
     author='Concordus Applications',
     author_email='support@concordusapps.com',

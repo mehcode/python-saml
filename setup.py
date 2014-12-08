90d3bab57fa300c5430e89e863c729996632e7d7
Ryan Leckey <leckey.ryan@gmail.com>
2014-12-08 11:55:34 -0800

    Add 3.4.x to the travis matrix.

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
     ],
     author='Concordus Applications',
     author_email='leckey.ryan@gmail.com',
